be3fcc97ec9aa9bcdaeb32e658cffd905af8c6d0
Oleksii Kochurov <aleksei.kochuroff@icloud.com>
2023-11-16 17:29:39 +0200

    handlers added

--- src/image-api.js
+++ src/image-api.js
@@ -3,11 +3,15 @@ import axios from 'axios';
 const API_KEY = '34183699-29109d6fbf2dd60241f6d6e15';
 const BASE_URL = 'https://pixabay.com/api/';
 
-export const fetchImages = async () => {
-  return axios.get(`${BASE_URL}?key=${API_KEY}`).then(response => {
-    if (!response.data) {
-      throw new Error();
-    }
-    return response.data;
-  });
+export const fetchImages = searchQuery => {
+  return axios
+    .get(
+      `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type="photo"&orientation="horizontal"&safesearch="true"`
+    )
+    .then(response => {
+      if (!response.data) {
+        throw new Error();
+      }
+      return response.data;
+    });
 };
--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 import { fetchImages } from './image-api';
 
@@ -7,8 +9,65 @@ const searchForm = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 
-const handleSubmit = () => {};
+const createMarkup = arrayOfImages => {
+  const markup = arrayOfImages
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        likes,
+        views,
+        comments,
+        downloads,
+        tags,
+      }) => `<div class="photo-card">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+    <div class="info">
+        <p class="info-item">
+            <b>Likes</b>
+            ${likes}
+        </p>
+            <p class="info-item">
+        <b>Views</b>
+        ${views}
+        </p>
+        <p class="info-item">
+            <b>Comments</b>
+            ${comments}
+        </p>
+        <p class="info-item">
+            <b>Downloads</b>
+            ${downloads}
+        </p>
+    </div>
+</div>`
+    )
+    .join('');
+  return markup;
+};
 
-fetchImages()
-  .then(data => console.log(data))
-  .catch(error => console.log(error.message));
+const handleSubmit = async event => {
+  event.preventDefault();
+  const { value } = event.currentTarget.elements.searchQuery;
+
+  try {
+    const { hits } = await fetchImages(value);
+    console.log(hits);
+    if (hits.length === 0) {
+      iziToast.warning({
+        message:
+          'Sorry, there are no images matching your search query. Please try again.',
+        position: 'topRight',
+      });
+    }
+
+    gallery.insertAdjacentHTML('beforeend', createMarkup(hits));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const onLoadMore = async () => {};
+
+searchForm.addEventListener('submit', handleSubmit);
+loadMoreBtn.addEventListener('click', onLoadMore);
